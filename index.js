64215a053900ad59c0c48d15c37c17a829db4bf9
Mathieu Garcia <mgarcia@wiseflat.com>
2014-12-11 23:13:30 +0100

    Update core

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var xplprowl = require("./lib/xpl-prowl");
 
 var wt = new xplprowl(null, {
-	xplSource: 'bnz-prowl.wiseflat',
-        prowlapi: 'MY-API-KEY'
+	xplSource: 'bnz-prowl.wiseflat'
 });
 
 wt._init(function(error, xpl) {
@@ -13,13 +12,13 @@ wt._init(function(error, xpl) {
 	}
         
         xpl.on("xpl:prowl.basic", function(evt) {
-		console.log("Receive message ", evt);
-                wt._push(evt);
+		//console.log("Receive message ", evt);
+                if(wt._schema_prowl_basic(evt.body)) wt._push(evt.body);
         }); 
         
         xpl.on("xpl:prowl.config", function(evt) {
-		console.log("Receive message ", evt);
-                wt._config(evt);
+		//console.log("Receive message ", evt);
+                if(wt._schema_prowl_config(evt.body)) wt._config(evt.body);
         }); 
 });
 
