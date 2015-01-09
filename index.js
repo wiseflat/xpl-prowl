05441f489b3c8ef587c206201097c8801c423632
Mathieu Garcia <mgarcia@wiseflat.com>
2015-01-09 23:01:08 +0100

    Upgrade core

--- index.js
+++ index.js
@@ -4,26 +4,33 @@ var wt = new xplprowl(null, {
 	//xplSource: 'bnz-prowl.wiseflat'
 });
 
-wt._init(function(error, xpl) {
+wt.init(function(error, xpl) {
 
 	if (error) {
 		console.error(error);
 		return;
 	}
         
+        // Load config file into hash
+        wt.readConfig();
+        
+        // Send every minutes an xPL status message 
+        setInterval(function(){
+                wt.sendConfig();
+        }, 60 * 1000);
+        
+        xpl.on("xpl:prowl.request", function(evt) {
+                if(evt.headerName == 'xpl-cmnd') wt.sendConfig();
+        });
+        
         xpl.on("xpl:prowl.basic", function(evt) {
 		console.log("Receive message ", evt);
                 if(evt.headerName == 'xpl-cmnd' && wt.validBasicSchema(evt.body)) wt.push(evt.body);
         }); 
         
-        xpl.on("xpl:prowl.config", function(evt) {
+        /*xpl.on("xpl:prowl.config", function(evt) {
 		console.log("Receive message ", evt);
                 if(evt.headerName == 'xpl-cmnd' && wt.validConfigSchema(evt.body)) wt.writeConfig(evt.body);
-        }); 
-
-        xpl.on("xpl:prowl.request", function(evt) {
-		console.log("Receive message ", evt);
-                if(evt.headerName == 'xpl-cmnd') wt.readConfig();
-        });
+        }); */
 });
 
